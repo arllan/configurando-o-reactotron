8a8efc6b2bf7674b668aa417b9f5ab6829bc7304
Arllan Pablo <arllanpablo@gmail.com>
2021-06-13 20:23:40 -0300

    adicionado a funcionaliade de benchmark e verificação de rede

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { View, Text } from 'react-native';
 
 import './config/ReactotronConfig'; // chama o arquivo de configuração
@@ -15,7 +15,38 @@ console.tron.display({
 })
 
 
+
 export default function exemplo_reactotron() {
+
+  const bench = libReactotron.benchmark("Verificar o tempo de execução") // instancia do marcador
+
+  function teste(){ // função a ser testada quanto tempo para ser executada
+    return 1000 *258
+  }
+ 
+  
+  function slowFunction() { // exemplo de como medir o tempo de execução de uma lib
+  
+    // Verifica quando tempo para executar essa funcionalidade
+    bench.step("Thing A")
+
+    bench.step(aula = '2589633');
+   
+    bench.step('TesteA: '+ teste());
+
+    // Verifica quando tempo para executar essa funcionalidade
+    bench.step("Thing B")
+  
+    // Verifica quando tempo para executar essa funcionalidade
+    bench.stop("Thing C")
+  }
+
+  bench.step('TesteB: '+ teste()); // função medida o tempo de execução
+
+  useEffect(()=>{
+    slowFunction()
+  },[])
+  
  return (
    <View>
      <Text>OII22I</Text>
--- config/ReactotronConfig.js
+++ config/ReactotronConfig.js
@@ -1,12 +1,15 @@
-import Reactotron from 'reactotron-react-native';
+import Reactotron, { networking } from 'reactotron-react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 if (__DEV__) {
   const tron = Reactotron.configure({ host: '192.168.18.24' })
     .useReactNative()
+    .use(networking())
     .setAsyncStorageHandler(AsyncStorage)
     .connect();
 
-  console.tron = tron;
+  libReactotron = Reactotron; // configuração para exporta a lib Reactotron
+
+  console.tron = tron; // configuração para modificar o indentificador do console
   tron.clear();
 }
